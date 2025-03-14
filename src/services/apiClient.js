158f26fe18bfe8b55bb7ef8a6af03926dec3ed7b
Olga Yefremova <lefrolg@gmail.com>
2025-03-14 11:35:15 +0200

    feat: set up cache storage in localStorage and define cache expiration time

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,10 +1,10 @@
 import Axios from "axios";
-import {setupCache} from "axios-cache-interceptor";
+import {setupCache, buildWebStorage} from "axios-cache-interceptor";
 
 const apiEndpoint = import.meta.env.VITE_NINJAS_API_URL;
 const apiKey = import.meta.env.VITE_NINJAS_API_KEY;
 
-const instance  = Axios.create({
+const instance = Axios.create({
   baseURL: apiEndpoint,
   headers: {
     "X-Api-Key": apiKey,
@@ -14,7 +14,9 @@ const instance  = Axios.create({
 
 const apiClient = setupCache(instance, {
   headerInterpreter: false,
-  cacheTakeover: false
+  cacheTakeover: false,
+  storage: buildWebStorage(localStorage, 'axios-cache:'),
+  ttl: 1000 * 60 * 60 * 24
 });
 
 export default apiClient;
